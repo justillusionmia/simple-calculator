f202a999c524c37752a06e8cd15f637833281672
justillusionmia <whiskeypierre002@gmail.com>
2024-12-17 12:28:17 -0400

    Create cal.js

---
+++ cal.js
@@ -0,0 +1,18 @@
+const display = document.getElementById('display');
+
+function clearDisplay() {
+    display.value = "";
+}
+
+function del() {
+    display.value = display.value.slice(0, -1)
+}
+ 
+function calculate() {
+    try {
+        display.value = eval(display.value);
+    }
+    catch(error) {
+        display.value = "Error";
+    }
+}
